81c6f134a70e7382e94d618deed183e1e47e6dfe
PowerOleg <geraskin.oleg@mail.ru>
2024-03-23 17:57:48 +0300

    Initial commit

---
+++ my_first_cmake_project/my_first_cmake_program/my_first_cmake_program.cpp
@@ -0,0 +1,12 @@
+﻿// my_first_cmake_program.cpp: определяет точку входа для приложения.
+//
+#include "my_first_cmake_program.h"
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, "ru");
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!";
+	return 0;
+}
